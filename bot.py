4a3695463459006cb7220640cd7774cd79dbc803
bernie-haxx <benkaranja43@gmail.com>
2020-09-17 15:06:36 +0300

    Implementation using the subclass of CLient and overriding its handler metohds

--- bot.py
+++ bot.py
@@ -5,25 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_NAME')
 
 
-client = discord.Client()
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has pulled the F up!')
 
-@client.event
-async def on_ready():
-    """
-    Discord.utils.find() is one utility that can improve the simplicity and readability of
-    this code by replacing the for loop with a inituitive, abstracted function:
-    
-    Also known as a predicate.
-    """
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} pulled the F up in to the following skreet:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'SKreet Members:\n - {members}')
+client = CustomClient()
 client.run(TOKEN)
+
