da943cf9b3c4c6c7003cb98715be9050620cad23
bernie-haxx <benkaranja43@gmail.com>
2020-09-17 15:15:09 +0300

    Adding the new members logic

--- bot.py
+++ bot.py
@@ -6,11 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+client = discord.Client()
 
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has pulled the F up!')
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has pulled the F up!')
 
-client = CustomClient()
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"It's Lit!!, {member.name} pulled up wit the cactus juice!"
+    )
+
 client.run(TOKEN)
-
