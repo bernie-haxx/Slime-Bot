9554faa8d6005c6840990a5e174d0dba10fd94e9
bernie-haxx <benkaranja43@gmail.com>
2020-09-17 15:43:12 +0300

    Message and Error Handler for the Discord Bot

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 
 import discord
 from dotenv import load_dotenv
@@ -18,5 +18,37 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f"It's Lit!!, {member.name} pulled up wit the cactus juice!"
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        'Bruh',
+        'I tink am bout to steeeaaaal!',
+        'OKEH',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+        '*top* Noice!',
+        "Heheaaa Boi!"
+    ]
+    if message.content == '69':
+         response = random.choice(quotes)
+         await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise dicord.DiscordException
 
 client.run(TOKEN)
