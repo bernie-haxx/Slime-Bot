00f7b6fdf4a77dc366452d3dc996d2908e10aab7
bernie-haxx <benkaranja43@gmail.com>
2020-09-17 14:18:49 +0300

    Phase 1 - connecting the bot to the server and looping through the servers connected to the bot. And also looping the users in the server.

--- bot.py
+++ bot.py
@@ -5,11 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_NAME')
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} pulled the F up!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} pulled the F up in to the following skreet:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'SKreet Members:\n - {members}')
 client.run(TOKEN)
